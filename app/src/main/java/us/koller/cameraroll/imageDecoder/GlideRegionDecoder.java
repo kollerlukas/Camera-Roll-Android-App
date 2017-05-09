d36a8d924c9650e9a4dd00287f56b95eb8bbeb01
Lukas <lukaskoller6@gmail.com>
2017-05-09 00:09:36 -0700

    - added a Custom ImageDecoder, which enables 8-Bit color - Bug fixes, when copying, moving or deleting files - code cleanup

--- app/src/main/java/us/koller/cameraroll/imageDecoder/GlideRegionDecoder.java
+++ app/src/main/java/us/koller/cameraroll/imageDecoder/GlideRegionDecoder.java
@@ -12,10 +12,15 @@ import com.davemorrissey.labs.subscaleview.decoder.ImageRegionDecoder;
 
 import java.io.InputStream;
 
+import us.koller.cameraroll.data.Settings;
+
+//inspired by https://gist.github.com/davemorrissey/e2781ba5b966c9e95539
 public class GlideRegionDecoder implements ImageRegionDecoder {
 
     private BitmapRegionDecoder decoder;
     private final Object decoderLock = new Object();
+
+    boolean use8BitColor;
 
     public GlideRegionDecoder() {
 
@@ -25,6 +30,7 @@ public class GlideRegionDecoder implements ImageRegionDecoder {
     public Point init(Context context, Uri uri) throws Exception {
         InputStream inputStream = context.getContentResolver().openInputStream(uri);
         this.decoder = BitmapRegionDecoder.newInstance(inputStream, false);
+        this.use8BitColor = Settings.getInstance(context).use8BitColor();
 
         return new Point(this.decoder.getWidth(), this.decoder.getHeight());
     }
@@ -34,7 +40,7 @@ public class GlideRegionDecoder implements ImageRegionDecoder {
         synchronized (this.decoderLock) {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inSampleSize = sampleSize;
-            options.inPreferredConfig = Bitmap.Config.RGB_565;
+            options.inPreferredConfig = use8BitColor ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
             Bitmap bitmap = this.decoder.decodeRegion(rect, options);
             if (bitmap == null) {
                 throw new RuntimeException("Region decoder returned null bitmap - image format may not be supported");
