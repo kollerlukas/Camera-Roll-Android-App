0c81c28695530cc69f54967d24828b62f8b68996
Lukas Koller <lukaskoller6@gmail.com>
2018-03-14 15:36:30 +0100

    - ExifEditorActivity: added Menu-Button to clear most of the ExifData (#153) + code clean up

--- app/src/main/java/us/koller/cameraroll/util/ExifUtil.java
+++ app/src/main/java/us/koller/cameraroll/util/ExifUtil.java
@@ -2,6 +2,9 @@ package us.koller.cameraroll.util;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.media.ExifInterface;
 
 import java.io.IOException;
@@ -22,8 +25,7 @@ public class ExifUtil {
     private static final int TYPE_RATIONAL = 3;
 
     //Tags
-    private static final String[] exifTags
-            = new String[]{
+    private static final String[] exifTags = {
             ExifInterface.TAG_APERTURE_VALUE,
             ExifInterface.TAG_ARTIST,
             ExifInterface.TAG_BITS_PER_SAMPLE,
@@ -161,8 +163,7 @@ public class ExifUtil {
     };
 
     //Types
-    private static final int[] exifTypes
-            = new int[]{
+    private static final int[] exifTypes = {
             TYPE_RATIONAL,
             TYPE_STRING,
             TYPE_INT,
@@ -300,8 +301,7 @@ public class ExifUtil {
     };
 
     //Values
-    private static final String[][] exifValues
-            = new String[][]{
+    private static final String[][] exifValues = {
             null,
             null,
             null,
@@ -451,6 +451,148 @@ public class ExifUtil {
             null
     };
 
+    // The Values that are removed when the Exif data is cleared
+    private static String[] valuesToRemove = {
+            ExifInterface.TAG_APERTURE_VALUE,
+            ExifInterface.TAG_ARTIST,
+            /*ExifInterface.TAG_BITS_PER_SAMPLE,*/
+            /*ExifInterface.TAG_BRIGHTNESS_VALUE,*/
+            /*ExifInterface.TAG_CFA_PATTERN,*/
+            /*ExifInterface.TAG_COLOR_SPACE,*/
+            /*ExifInterface.TAG_COMPONENTS_CONFIGURATION,*/
+            /*ExifInterface.TAG_COMPRESSED_BITS_PER_PIXEL,*/
+            /*ExifInterface.TAG_COMPRESSION,*/
+            /*ExifInterface.TAG_CONTRAST,*/
+            ExifInterface.TAG_COPYRIGHT,
+            /*ExifInterface.TAG_CUSTOM_RENDERED,*/
+            ExifInterface.TAG_DATETIME,
+            ExifInterface.TAG_DATETIME_DIGITIZED,
+            ExifInterface.TAG_DATETIME_ORIGINAL,
+            /*ExifInterface.TAG_DEFAULT_CROP_SIZE,*/
+            ExifInterface.TAG_DEVICE_SETTING_DESCRIPTION,
+            ExifInterface.TAG_DIGITAL_ZOOM_RATIO,
+            /*ExifInterface.TAG_DNG_VERSION,*/
+            /*ExifInterface.TAG_EXIF_VERSION,*/
+            ExifInterface.TAG_EXPOSURE_BIAS_VALUE,
+            ExifInterface.TAG_EXPOSURE_INDEX,
+            ExifInterface.TAG_EXPOSURE_MODE,
+            ExifInterface.TAG_EXPOSURE_PROGRAM,
+            ExifInterface.TAG_EXPOSURE_TIME,
+            ExifInterface.TAG_FILE_SOURCE,
+            ExifInterface.TAG_FLASH,
+            ExifInterface.TAG_FLASHPIX_VERSION,
+            ExifInterface.TAG_FLASH_ENERGY,
+            ExifInterface.TAG_FOCAL_LENGTH,
+            ExifInterface.TAG_FOCAL_LENGTH_IN_35MM_FILM,
+            ExifInterface.TAG_FOCAL_PLANE_RESOLUTION_UNIT,
+            ExifInterface.TAG_FOCAL_PLANE_X_RESOLUTION,
+            ExifInterface.TAG_FOCAL_PLANE_Y_RESOLUTION,
+            ExifInterface.TAG_F_NUMBER,
+            ExifInterface.TAG_GAIN_CONTROL,
+            ExifInterface.TAG_GPS_ALTITUDE,
+            ExifInterface.TAG_GPS_ALTITUDE_REF,
+            ExifInterface.TAG_GPS_AREA_INFORMATION,
+            ExifInterface.TAG_GPS_DATESTAMP,
+            ExifInterface.TAG_GPS_DEST_BEARING,
+            ExifInterface.TAG_GPS_DEST_BEARING_REF,
+            ExifInterface.TAG_GPS_DEST_DISTANCE,
+            ExifInterface.TAG_GPS_DEST_DISTANCE_REF,
+            ExifInterface.TAG_GPS_DEST_LATITUDE,
+            ExifInterface.TAG_GPS_DEST_LATITUDE_REF,
+            ExifInterface.TAG_GPS_DEST_LONGITUDE,
+            ExifInterface.TAG_GPS_DEST_LONGITUDE_REF,
+            ExifInterface.TAG_GPS_DIFFERENTIAL,
+            ExifInterface.TAG_GPS_DOP,
+            ExifInterface.TAG_GPS_IMG_DIRECTION,
+            ExifInterface.TAG_GPS_IMG_DIRECTION_REF,
+            ExifInterface.TAG_GPS_LATITUDE,
+            ExifInterface.TAG_GPS_LATITUDE_REF,
+            ExifInterface.TAG_GPS_LONGITUDE,
+            ExifInterface.TAG_GPS_LONGITUDE_REF,
+            ExifInterface.TAG_GPS_MAP_DATUM,
+            ExifInterface.TAG_GPS_MEASURE_MODE,
+            ExifInterface.TAG_GPS_PROCESSING_METHOD,
+            ExifInterface.TAG_GPS_SATELLITES,
+            ExifInterface.TAG_GPS_SPEED,
+            ExifInterface.TAG_GPS_SPEED_REF,
+            ExifInterface.TAG_GPS_STATUS,
+            ExifInterface.TAG_GPS_TIMESTAMP,
+            ExifInterface.TAG_GPS_TRACK,
+            ExifInterface.TAG_GPS_TRACK_REF,
+            ExifInterface.TAG_GPS_VERSION_ID,
+            ExifInterface.TAG_IMAGE_DESCRIPTION,
+            /*ExifInterface.TAG_IMAGE_LENGTH,*/
+            ExifInterface.TAG_IMAGE_UNIQUE_ID,
+            /*ExifInterface.TAG_IMAGE_WIDTH,*/
+            ExifInterface.TAG_INTEROPERABILITY_INDEX,
+            ExifInterface.TAG_PHOTOGRAPHIC_SENSITIVITY,
+            /*ExifInterface.TAG_JPEG_INTERCHANGE_FORMAT,*/
+            /*ExifInterface.TAG_JPEG_INTERCHANGE_FORMAT_LENGTH,*/
+            ExifInterface.TAG_LIGHT_SOURCE,
+            ExifInterface.TAG_MAKE,
+            ExifInterface.TAG_MAKER_NOTE,
+            ExifInterface.TAG_MAX_APERTURE_VALUE,
+            ExifInterface.TAG_METERING_MODE,
+            ExifInterface.TAG_MODEL,
+            ExifInterface.TAG_NEW_SUBFILE_TYPE,
+            ExifInterface.TAG_OECF,
+            ExifInterface.TAG_ORF_ASPECT_FRAME,
+            ExifInterface.TAG_ORF_PREVIEW_IMAGE_LENGTH,
+            ExifInterface.TAG_ORF_PREVIEW_IMAGE_START,
+            ExifInterface.TAG_ORF_THUMBNAIL_IMAGE,
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.TAG_PHOTOMETRIC_INTERPRETATION,
+            /*ExifInterface.TAG_PIXEL_X_DIMENSION,*/
+            /*ExifInterface.TAG_PIXEL_Y_DIMENSION,*/
+            ExifInterface.TAG_PLANAR_CONFIGURATION,
+            ExifInterface.TAG_PRIMARY_CHROMATICITIES,
+            ExifInterface.TAG_REFERENCE_BLACK_WHITE,
+            ExifInterface.TAG_RELATED_SOUND_FILE,
+            ExifInterface.TAG_RESOLUTION_UNIT,
+            ExifInterface.TAG_ROWS_PER_STRIP,
+            ExifInterface.TAG_RW2_ISO,
+            ExifInterface.TAG_RW2_JPG_FROM_RAW,
+            ExifInterface.TAG_RW2_SENSOR_BOTTOM_BORDER,
+            ExifInterface.TAG_RW2_SENSOR_LEFT_BORDER,
+            ExifInterface.TAG_RW2_SENSOR_RIGHT_BORDER,
+            ExifInterface.TAG_RW2_SENSOR_TOP_BORDER,
+            ExifInterface.TAG_SAMPLES_PER_PIXEL,
+            ExifInterface.TAG_SATURATION,
+            ExifInterface.TAG_SCENE_CAPTURE_TYPE,
+            ExifInterface.TAG_SCENE_TYPE,
+            ExifInterface.TAG_SENSING_METHOD,
+            ExifInterface.TAG_SHARPNESS,
+            ExifInterface.TAG_SHUTTER_SPEED_VALUE,
+            ExifInterface.TAG_SOFTWARE,
+            ExifInterface.TAG_SPATIAL_FREQUENCY_RESPONSE,
+            ExifInterface.TAG_SPECTRAL_SENSITIVITY,
+            ExifInterface.TAG_STRIP_BYTE_COUNTS,
+            ExifInterface.TAG_STRIP_OFFSETS,
+            ExifInterface.TAG_SUBFILE_TYPE,
+            ExifInterface.TAG_SUBJECT_AREA,
+            ExifInterface.TAG_SUBJECT_DISTANCE,
+            ExifInterface.TAG_SUBJECT_DISTANCE_RANGE,
+            ExifInterface.TAG_SUBJECT_LOCATION,
+            ExifInterface.TAG_SUBSEC_TIME,
+            ExifInterface.TAG_SUBSEC_TIME_DIGITIZED,
+            ExifInterface.TAG_SUBSEC_TIME_ORIGINAL,
+            ExifInterface.TAG_THUMBNAIL_IMAGE_LENGTH,
+            ExifInterface.TAG_THUMBNAIL_IMAGE_WIDTH,
+            ExifInterface.TAG_TRANSFER_FUNCTION,
+            ExifInterface.TAG_USER_COMMENT,
+            ExifInterface.TAG_WHITE_BALANCE,
+            ExifInterface.TAG_WHITE_POINT,
+            ExifInterface.TAG_X_RESOLUTION,
+            ExifInterface.TAG_Y_CB_CR_COEFFICIENTS,
+            ExifInterface.TAG_Y_CB_CR_POSITIONING,
+            ExifInterface.TAG_Y_CB_CR_SUB_SAMPLING,
+            ExifInterface.TAG_Y_RESOLUTION
+    };
+
+    public interface Callback {
+        void done(boolean success);
+    }
+
     public static ExifInterface getExifInterface(Context context, AlbumItem albumItem) {
         if (albumItem == null) {
             return null;
@@ -494,7 +636,7 @@ public class ExifUtil {
         return castValue(tag, value);
     }
 
-    public static Object castValue(String tag, String value)
+    private static Object castValue(String tag, String value)
             throws NumberFormatException, NullPointerException {
         if (value == null || value.equals("")) {
             return null;
@@ -569,12 +711,74 @@ public class ExifUtil {
         }
     }
 
-    public static class ExifItem {
+    @SuppressWarnings("unused")
+    public static void saveChanges(ExifInterface exifInterface, List<ExifItem> editedItems) {
+        saveChanges(exifInterface, editedItems, null);
+    }
+
+    public static void saveChanges(final ExifInterface exifInterface,
+                                   final List<ExifItem> editedItems, final Callback callback) {
+        if (exifInterface == null) {
+            return;
+        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                boolean success = true;
+                for (ExifItem item : editedItems) {
+                    exifInterface.setAttribute(item.getTag(), item.getValue());
+                }
+                try {
+                    exifInterface.saveAttributes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    success = false;
+                }
+
+                if (callback != null) {
+                    callback.done(success);
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("unused")
+    public static void removeExifData(ExifInterface exifInterface) {
+        removeExifData(exifInterface, null);
+    }
+
+    public static void removeExifData(final ExifInterface exifInterface, final Callback callback) {
+        if (exifInterface == null) {
+            return;
+        }
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                boolean success = true;
+                // remove all Exif data
+                for (String tag : valuesToRemove) {
+                    exifInterface.setAttribute(tag, null);
+                }
+                try {
+                    exifInterface.saveAttributes();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    success = false;
+                }
+
+                if (callback != null) {
+                    callback.done(success);
+                }
+            }
+        });
+    }
+
+    public static class ExifItem implements Parcelable {
 
         private String tag;
         private String value;
 
-        ExifItem(String tag, String value) {
+        public ExifItem(String tag, String value) {
             this.tag = tag;
             this.value = value;
         }
@@ -587,9 +791,38 @@ public class ExifUtil {
             return value;
         }
 
+        public void setValue(String value) {
+            this.value = value;
+        }
+
         @Override
         public String toString() {
             return "(Tag: " + tag + ", Value: " + value + ")";
+        }
+
+        public static final Parcelable.Creator<ExifItem> CREATOR = new Parcelable.Creator<ExifItem>() {
+            @Override
+            public ExifItem createFromParcel(Parcel in) {
+                String tag = in.readString();
+                String value = in.readString();
+                return new ExifItem(tag, value);
+            }
+
+            @Override
+            public ExifItem[] newArray(int size) {
+                return new ExifItem[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            parcel.writeString(tag);
+            parcel.writeString(value);
         }
     }
 }
