465eb6be7c189533ba344ad4b104bc26e46b5992
Lukas Koller <lukaskoller6@gmail.com>
2018-04-04 16:40:44 +0200

    - fixed #165, #166, #167

--- app/src/main/java/us/koller/cameraroll/data/fileOperations/FileOperation.java
+++ app/src/main/java/us/koller/cameraroll/data/fileOperations/FileOperation.java
@@ -10,7 +10,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
