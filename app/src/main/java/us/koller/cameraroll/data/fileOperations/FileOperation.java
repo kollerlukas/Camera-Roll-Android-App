d84a9b6a63cc0afbd5fd1295bf52ebed9c29ae03
Lukas Koller <lukaskoller6@gmail.com>
2018-04-04 16:40:28 +0200

    - fixed #165, #166, #167

--- app/src/main/java/us/koller/cameraroll/data/fileOperations/FileOperation.java
+++ app/src/main/java/us/koller/cameraroll/data/fileOperations/FileOperation.java
@@ -10,6 +10,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -120,9 +121,13 @@ public abstract class FileOperation extends IntentService implements Parcelable 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(getApplicationContext());
         }
-        return new NotificationCompat.Builder(this, getString(R.string.file_op_channel_id))
-                .setContentTitle(getNotificationTitle())
-                .setSmallIcon(getNotificationSmallIconRes());
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,
+                getString(R.string.file_op_channel_id))
+                .setContentTitle(getNotificationTitle());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder.setSmallIcon(getNotificationSmallIconRes());
+        }
+        return builder;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -428,8 +433,10 @@ public abstract class FileOperation extends IntentService implements Parcelable 
             }
             final NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(context,
                     context.getString(R.string.file_op_channel_id))
-                    .setContentTitle("Scanning...")
-                    .setSmallIcon(R.drawable.ic_autorenew_white);
+                    .setContentTitle("Scanning...");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                notifBuilder.setSmallIcon(R.drawable.ic_autorenew_white);
+            }
             notifBuilder.setProgress(paths.length, 0, false);
             final NotificationManager manager =
                     (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
